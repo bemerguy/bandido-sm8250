a6391f22cf0472719432ecfbf4d4c859bdd87ea0
Minchan Kim <minchan@google.com>
2019-07-25 15:52:01 +0900

    memlat: don't read perf event if other CPU is under scm call
    memlat works based on perf event to calculate algorithm.
    Perf event needs IPI to read count from other CPU.
    However, if the CPU is under heavy scm call(+20ms), it could
    be stuck until the CPU could complete the scm call.
    It would cause long latency.
    
    This patch adds global atomic count to check whether scm is
    going. If it's under going, memlat doesn't invoke perf event
    function to avoid IPI stuck. Although there is still race to
    check it, the race should be small enough so effectivey, it
    mitigate such long latency.
    
    Please check to memlat owner what happens when read_event
    sometimes returns zero due to scm call in parallel.
    
    Bug: 134440633
    Change-Id: I0403d151ededcc80d531c8cd81daa783000902d1
    Signed-off-by: Minchan Kim <minchan@google.com>
    [lazerl0rd: Cleaned up the patch slightly.]
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: dreamisbaka <jolinux.g@gmail.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- drivers/soc/qcom/scm.c
+++ drivers/soc/qcom/scm.c
@@ -106,6 +106,7 @@ static int __scm_call_armv8_64(u64 x0, u64 x1, u64 x2, u64 x3, u64 x4, u64 x5,
 	register u64 r5 asm("x5") = x5;
 	register u64 r6 asm("x6") = 0;
 
+	atomic_inc(&scm_call_count);
 	do {
 		asm volatile(
 			__asmeq("%0", R0_STR)
@@ -139,6 +140,8 @@ static int __scm_call_armv8_64(u64 x0, u64 x1, u64 x2, u64 x3, u64 x4, u64 x5,
 #endif
 	} while (r0 == SCM_INTERRUPTED);
 
+	atomic_dec(&scm_call_count);
+
 	if (ret1)
 		*ret1 = r1;
 	if (ret2)
@@ -160,6 +163,7 @@ static int __scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 	register u32 r5 asm("w5") = w5;
 	register u32 r6 asm("w6") = 0;
 
+	atomic_inc(&scm_call_count);
 	do {
 		asm volatile(
 			__asmeq("%0", R0_STR)
@@ -194,6 +198,8 @@ static int __scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 
 	} while (r0 == SCM_INTERRUPTED);
 
+	atomic_dec(&scm_call_count);
+
 	if (ret1)
 		*ret1 = r1;
 	if (ret2)
@@ -217,6 +223,7 @@ static int __scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 	register u32 r5 asm("r5") = w5;
 	register u32 r6 asm("r6") = 0;
 
+	atomic_inc(&scm_call_count);
 	do {
 		asm volatile(
 			__asmeq("%0", R0_STR)
@@ -243,6 +250,8 @@ static int __scm_call_armv8_32(u32 w0, u32 w1, u32 w2, u32 w3, u32 w4, u32 w5,
 			 "r" (r5), "r" (r6));
 
 	} while (r0 == SCM_INTERRUPTED);
+
+	atomic_dec(&scm_call_count);
 
 	if (ret1)
 		*ret1 = r1;
