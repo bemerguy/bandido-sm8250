f74d967b5248ef229fb6fe8549a38c5f622ae100
LaKard0 <kishenko2017@gmail.com>
2022-02-20 11:39:59 +0300

    Code quality fixes
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -592,30 +592,6 @@ int main(int ac, char **av)
 				defconfig_file);
 			exit(1);
 		}
-
-		name = getenv("KCONFIG_VARIANT");
-		if (name) {
-			if (conf_read_simple(name, S_DEF_USER, false)) {
-				fprintf(stderr,
-					"***\n"
-					  "*** Can't find variant configuration \"%s\"!\n"
-					  "***\n",
-					name);
-				exit(1);
-			}
-		}
-
-		name = getenv("KCONFIG_DEBUG");
-		if (name) {
-			if (conf_read_simple(name, S_DEF_USER, false)) {
-				fprintf(stderr,
-					"***\n"
-					  "*** Can't find debug configuration \"%s\"!\n"
-					  "***\n",
-					name);
-				exit(1);
-			}
-		}
 		break;
 	case savedefconfig:
 	case syncconfig:
