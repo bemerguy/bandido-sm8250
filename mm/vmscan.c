793c252ba926ffdd8d022ad174d972d624e31818
Yu Zhao <yuzhao@google.com>
2022-06-14 22:47:20 -0600

    mglru: fixes
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Change-Id: I09999793fd6481bcf2acefb29c57d5d40c0b1427

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4275,7 +4275,7 @@ static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 	if (!success) {
 		pr_err("mglru: min_ttl unsatisfied, calling OOM killer\n");
 		lru_gen_min_ttl_unsatisfied++;
-		if (mutex_trylock(&oom_lock)) {
+		if (!sc->order && mutex_trylock(&oom_lock)) {
 			struct oom_control oc = {
 				.gfp_mask = sc->gfp_mask,
 				.order = sc->order,
