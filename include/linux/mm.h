05d2cc1f85367daa2d3985757c2e26092c88793e
bemerguy <heiler.bemerguy@gmail.com>
2022-12-18 17:52:03 -0300

    bump max_readahead to 256kb

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2574,7 +2574,7 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
+#define VM_MAX_READAHEAD	256	/* kbytes */
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
