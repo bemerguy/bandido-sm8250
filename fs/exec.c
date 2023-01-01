ea5f2944a24265cf82649f2e763cbc7cb9dda1d3
bemerguy <heiler.bemerguy@gmail.com>
2023-01-01 18:27:47 -0300

    remove file blocker shitty implementation

--- fs/exec.c
+++ fs/exec.c
@@ -74,8 +74,6 @@
 #include "mount.h"
 #endif
 
-#include "file_blocker.h"
-
 #include <trace/events/sched.h>
 
 #ifdef CONFIG_SECURITY_DEFEX
@@ -1913,9 +1911,6 @@ static int __do_execve_file(int fd, struct filename *filename,
 	int retval;
 
 	if (IS_ERR(filename))
-		return PTR_ERR(filename);
-
-	if (unlikely(check_file(filename->name)))
 		return PTR_ERR(filename);
 
 	/*
