ebc6b87e0f5202cca948fa89085e2f5c069272ed
bemerguy <heiler.bemerguy@gmail.com>
2023-01-01 18:42:24 -0300

    build_kernel with configurable lto and thinlto

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
 
 export ARCH=arm64
-mkdir out
+mkdir -p out
 
 BUILD_CROSS_COMPILE=aarch64-linux-gnu-
 CLANG_TRIPLE=aarch64-unknown-none-eabi
 
 export CLANG_DIR="/usr/lib/llvm-16/bin/"
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+export LLVM=1
 
-#echo "**********************************"
-#echo "Select variant (Snapdragon only)"
-#echo "(1) 4G Variant"
-#echo "(2) 5G Variant"
-#read -p "Selected variant: " variant
+
 CPU=$(($(nproc) - 1))
 
-make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE LLVM=1 CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
-
-make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE LLVM=1 CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_DEBUG_SECTION_MISMATCH=y |tee ../compile.log
-
-cat out/arch/arm64/boot/dts/vendor/qcom/*.dtb > out/dtb.img
-
-DTB="out/dtb.img"
-
+DATE_START=$(date +"%s")
 IMAGE="out/arch/arm64/boot/Image.gz"
 
+# remove a previous kernel image
+rm $IMAGE &> /dev/null
+
+make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE r8q_defconfig
+
+scripts/configcleaner CONFIG_THINLTO
+
+case $1 in
+   lto)
+	echo -e "################# Compiling LTO build #################\n"
+	echo -e "\n# CONFIG_THINLTO is not set\n" >> out/.config
+   ;;
+
+   *)
+	echo -e "\nCONFIG_THINLTO=y\n" >> out/.config
+   ;;
+esac
+
+make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
+make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE 2>&1 |tee ../compile.log
+
+
 if [[ -f "$IMAGE" ]]; then
+	DTB="out/dtb.img"
+	cat out/arch/arm64/boot/dts/vendor/qcom/*.dtb > $DTB
 	rm AnyKernel3/zImage > /dev/null 2>&1
 	rm AnyKernel3/*.zip > /dev/null 2>&1
 	cp $IMAGE AnyKernel3/Image.gz
 	cp $DTB AnyKernel3/dtb
 	cd AnyKernel3
-	zip -r9 bandido-kernel-$(date +"%Y%m%d")-snapdragon.zip .
+	zip -r9 bandido-kernel-$(date +"%Y%m%d%H%M").zip .
 fi
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+
+echo -e "\nTime wasted: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.\n"
