49bce9540127682ea11ddbe76446de1d322c85cc
bemerguy <heiler.bemerguy@gmail.com>
2023-08-28 17:10:21 -0300

    update our build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 unset LLVM
-export ARCH=arm64
 
-BUILD_CROSS_COMPILE=/root/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
+#BUILD_CROSS_COMPILE=/root/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
+BUILD_CROSS_COMPILE=/root/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+BUILD_CROSS_COMPILE=/home/me/x-tools/aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
+#uncomment those to use clang instead
+export LLVM=1
 export CLANG_DIR="/usr/lib/llvm-16/bin/"
-
-#where to really use clang/llvm or not?
-#export LLVM=1
 
 #######################################
 mkdir -p out
+export ARCH=arm64
 CLANG_TRIPLE=aarch64-unknown-none-eabi
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
-CPU=$(($(nproc) - 1))
+CPU=$(($(nproc) - 2))
 
 DATE_START=$(date +"%s")
-IMAGE="out/arch/arm64/boot/Image.gz"
-DTB="out/dtb.img"
+IMAGE="out/arch/arm64/boot/Image.gz-dtb"
 
 # remove a previous kernel image
 rm $IMAGE &> /dev/null
@@ -54,8 +55,10 @@ else
    esac
 fi
 
-make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
-make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CLANG_TRIPLE=$CLANG_TRIPLE 2>&1 |tee ../compile-bandido.log
+make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+	CLANG_TRIPLE=$CLANG_TRIPLE oldconfig
+make -j$CPU -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+	CLANG_TRIPLE=$CLANG_TRIPLE 2>&1 |tee ../compile-bandido.log
 
 
 if [[ -f "$IMAGE" ]]; then
@@ -64,12 +67,11 @@ if [[ -f "$IMAGE" ]]; then
 		adb reboot recovery
 	fi
 	KERNELZIP="bandido-kernel-$(date +"%Y%m%d%H%M").zip"
-	cat out/arch/arm64/boot/dts/vendor/qcom/*.dtb > $DTB
-	rm AnyKernel3/zImage > /dev/null 2>&1
-	rm AnyKernel3/Image > /dev/null 2>&1
+#	cat out/arch/arm64/boot/dts/vendor/qcom/*.dtb > $DTB
+	rm AnyKernel3/*Image* > /dev/null 2>&1
 	rm AnyKernel3/*.zip > /dev/null 2>&1
 	cp $IMAGE AnyKernel3/
-	cp $DTB AnyKernel3/dtb
+#	cp $DTB AnyKernel3/dtb
 	cd AnyKernel3
 
 	zip -r9 $KERNELZIP .
@@ -81,5 +83,5 @@ if [[ -f "$IMAGE" ]]; then
 	adb wait-for-recovery
 	adb push $KERNELZIP /sdcard/
 else
-	echo -e "ERROR\n"
+	echo -e "\nERROR. Something broke along the way since $IMAGE is not there\n"
 fi
