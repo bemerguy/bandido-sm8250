99dbd60e32a1cc6e0a426a9ee494628a174f96c7
bemerguy <heiler.bemerguy@gmail.com>
2023-09-14 18:23:07 -0300

    adreno_tz add some unlikelies

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -31,10 +31,10 @@ static DEFINE_SPINLOCK(tz_lock);
 #define MAX_TZ_VERSION		0
 
 /*
- * CEILING is 50msec, larger than any standard
+ * CEILING is 40msec, larger than any standard
  * frame length, but less than the idle timer.
  */
-#define CEILING			50000
+#define CEILING			40000
 #define TZ_RESET_ID		0x3
 #define TZ_UPDATE_ID		0x4
 #define TZ_INIT_ID		0x6
@@ -392,7 +392,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 	}
 
 	level = devfreq_get_freq_level(devfreq, stats->current_frequency);
-	if (level < 0) {
+	if (unlikely(level < 0)) {
 		pr_err(TAG "bad freq %ld\n", stats->current_frequency);
 		return level;
 	}
@@ -402,7 +402,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 	 * increase frequency.  Otherwise run the normal algorithm.
 	 */
 	if (!priv->disable_busy_time_burst &&
-			priv->bin.busy_time > CEILING) {
+			unlikely(priv->bin.busy_time > CEILING)) {
 		val = -1 * level;
 	} else {
 
