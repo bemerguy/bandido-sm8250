60c9f77714dcd3a034ad39460652204b2990f315
bemerguy <heiler.bemerguy@gmail.com>
2023-10-11 16:15:24 -0300

    mm: page_alloc fix warning

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4619,10 +4619,10 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	int retry_loop_count = 0;
 	unsigned long jiffies_s = jiffies;
 	u64 utime, stime_s, stime_e, stime_d;
+        bool woke_kswapd = false;
+        bool used_vmpressure = false;
 
 	task_cputime(current, &utime, &stime_s);
-	bool woke_kswapd = false;
-	bool used_vmpressure = false;
 
 	/*
 	 * We also sanity check to catch abuse of atomic reserves being used by
