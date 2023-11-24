4e09c7c1ae02fd4e6a1d671c25b748e278c7660d
bemerguy <heiler.bemerguy@gmail.com>
2023-11-24 14:01:37 -0300

    Squashed G781BXXS8HWI3 stuff

--- security/samsung/mz/mz_page_v5_10.c
+++ security/samsung/mz/mz_page_v5_10.c
@@ -181,6 +181,7 @@ MzResult mz_migrate_and_pin(struct page *target_page, unsigned long va, uint8_t 
 	//pin
 	mz_ret = mz_get_user_pages(task, mm, va, &(new_page[0]), 0, buf);
 	if (mz_ret != MZ_SUCCESS) {
+		if(new_page[0] != NULL)
 		put_page(new_page[0]);
 		goto out_pfns;
 	}
