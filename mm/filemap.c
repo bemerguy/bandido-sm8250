7d2cf57fa2035b563afab3186cd34d1091b4bcbe
bemerguy <heiler.bemerguy@gmail.com>
2024-06-01 09:25:49 -0300

    filemap: undo samsung read_ahead changes

--- mm/filemap.c
+++ mm/filemap.c
@@ -2623,7 +2623,7 @@ static struct file *do_sync_mmap_readahead(struct vm_fault *vmf)
 	struct address_space *mapping = file->f_mapping;
 	struct file *fpin = NULL;
 	pgoff_t offset = vmf->pgoff;
-	unsigned int ra_pages;
+//	unsigned int ra_pages;
 
 	/* If we don't want any read-ahead, don't bother */
 	if (vmf->vma_flags & VM_RAND_READ)
@@ -2655,11 +2655,10 @@ static struct file *do_sync_mmap_readahead(struct vm_fault *vmf)
 	 * mmap read-around
 	 */
 	fpin = maybe_unlock_mmap_for_io(vmf, fpin);
-	ra_pages = min_t(unsigned int, ra->ra_pages, mmap_readaround_limit);
-	ra->start = max_t(long, 0, offset - ra_pages / 2);
-	ra->size = ra_pages;
-	ra->async_size = ra_pages / 4;
-	filemap_tracing_mark_begin(file, offset, ra_pages, 1);
+	ra->start = max_t(long, 0, offset - ra->ra_pages / 2);
+	ra->size = ra->ra_pages;
+	ra->async_size = ra->ra_pages / 4;
+	filemap_tracing_mark_begin(file, offset, ra->ra_pages, 1);
 	ra_submit(ra, mapping, file);
 	filemap_tracing_mark_end();
 	return fpin;
