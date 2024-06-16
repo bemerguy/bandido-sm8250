06aee6e60ae4f44fd24f9396dc50d112e9a58161
bemerguy <heiler.bemerguy@gmail.com>
2024-06-16 10:42:26 -0300

    fixup: ext4 remove tracing

--- fs/ext4/readpage.c
+++ fs/ext4/readpage.c
@@ -215,24 +215,6 @@ ext4_submit_bio_read(struct bio *bio)
 #ifdef CONFIG_FS_HPB
 	struct inode *inode = bio->bi_io_vec[0].bv_page->mapping->host;
 #endif
-	if (trace_android_fs_dataread_start_enabled()) {
-		struct page *first_page = bio->bi_io_vec[0].bv_page;
-
-		if (first_page != NULL) {
-			char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-			path = android_fstrace_get_pathname(pathbuf,
-						    MAX_TRACE_PATHBUF_LEN,
-						    first_page->mapping->host);
-			trace_android_fs_dataread_start(
-				first_page->mapping->host,
-				page_offset(first_page),
-				bio->bi_iter.bi_size,
-				current->pid,
-				path,
-				current->comm);
-		}
-	}
 #ifdef CONFIG_FS_HPB
 	if(ext4_test_inode_state(inode, EXT4_STATE_HPB))
 		bio->bi_opf |= REQ_HPB_PREFER;
